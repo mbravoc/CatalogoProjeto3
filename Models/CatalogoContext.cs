e48b4879c7938bf7f6da8bf31eb27cb956773d51
Matheus Correa <matheuscorreabravo@gmail.com>
2019-08-08 10:08:54 -0300

    Alterações tela login

---
+++ Models/CatalogoContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace catologoEm3.Models
+{
+    public class CatalogoContext : DbContext
+    {
+        public DbSet<Usuario> Usuario { get; set; }
+
+        public DbSet<Cliente> Ciente { get; set; }
+        public DbSet<Carro> Carro { get; set; }
+        public DbSet<Tipo> Tipo { get; set; }
+        public DbSet<Pecas> Pecas { get; set; }
+
+        public CatalogoContext(DbContextOptions<CatalogoContext> options) : base(options) {
+
+        }
+
+    }
+
+}
